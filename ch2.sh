2abc42361eae57a4bf148d3594032638ef076b79
Mr-blues-bits <140849446+Mr-blues-bits@users.noreply.github.com>
2023-07-28 23:11:05 +0100

    ch2.sh

---
+++ ch2.sh
@@ -0,0 +1,6 @@
+echo "Arch Root Changed to Installation"
+echo #
+echo "Enabling Network Manager:..."
+systemctl enable NetworkManager
+echo "Enabling Avhil Service:..."
+systemctl enable avahi-daemon
