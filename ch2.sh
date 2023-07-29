c67e8b92d8f06ac4d56bbe0c12dcbd49d28474f3
Mr-blues-bits <140849446+Mr-blues-bits@users.noreply.github.com>
2023-07-29 12:04:19 +0100

    Update ch2.sh

--- ch2.sh
+++ ch2.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+source logos.sh
 
-echo "Arch Root Changed to Installation"
+grubLogo
+
+echo #
+
+while true; do
+
+read -p "Do you want to install Grub? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo ok, we will proceed;
+	 echo "Installing Grub on /boot/efi."
+         grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+	       echo "Make grub config file:"
+	       grub-mkconfig -o /boot/grub/grub.cfg	
+  
+  break;;
+	[nN] ) echo exiting...;
+		break;;
+	* ) echo invalid response;;
+esac
+
+done
+
 echo #
 echo "Enabling Network Manager:..."
 systemctl enable NetworkManager
