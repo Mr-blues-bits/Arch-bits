72fb619d62c4bdb950e0f962d6cfb536d729fe61
Mr-blues-bits <140849446+Mr-blues-bits@users.noreply.github.com>
2023-08-19 19:12:47 +0100

    Update Base.sh

--- Base.sh
+++ Base.sh
@@ -1,72 +1,38 @@
 #!/bin/bash
+
 source logos.sh
+source ff.sh
+#source logos.sh
+
+clear
+
+bitsLogo
 
 echo "Welcome to Arch Install Script:"
 
-bitsLogo
 echo #
-# Call the function to show the selection menu
+
+# Call the function to show the Disk selection menu
+
 show_disk_menu
-echo #
-#lsblk -no NAME,FSTYPE,SIZE,MOUNTPOINT $diskSel
-
-if [ "$(lsblk -no FSTYPE $diskSel)" != "" ]; then
- select_partition "Root"
-  #read -p "Select root partition: " root_part
- select_partition "SWAP"
-  #read -p "Select swap partition: " swap_part
- select_partition "Boot"
-  #read -p "Select boot partition: " boot_part
-fi
-
-clear
-echo #
-	echo "Final Disk Layout"
-	disksLogo
-
-	echo #
-	echo "Installing on $diskSel:"
-	echo "Selected Root =========>>:$root_part"
-	echo "Selected SWAP =========>>:$swap_part"
-	echo "Selected Boot =========>>:$boot_part"
-echo #
-echo #
-
-read -p "Please type 'yes' if you are happy with disk layout: " answer
-    if [ "$answer" != "yes" ]; then
-        echo "Exiting script."
-        exit 1
-    fi
-echo #
-echo #
-	mount $root_part /mnt
-	echo "Mounted $root_part at /mnt"
-	mkdir -p "/mnt/boot/efi"
 
 echo #
-mount $boot_part /mnt/boot/efi
-echo "Mounted $boot_part to /mnt/boot/efi"
+final_disk_layout
+ch_File_SystemMenu
+set_filesystem
 
-echo #
-swapon $swap_part
-echo "Swap set on $swap_part"
 
-echo "Disk section completed"
+lsblk echo "/dev/$disk" -f
+echo "DeBUG Point....This is final layout for this install: "
+read -p "exit now: " exitnow
 
-areyousureLogo
 
-read -p "Now making changes to the system. Type yes to continue:" answer
-    if [ "$answer" != "yes" ]; then
-      	swapoff swap_part
- 	umount -a
-       echo "Exiting script."
-       exit 1
-    fi
+#areyousureLogo
 
 basesystemLogo
 echo "Base packages selected for this install:"
 echo #
-echo "base base-devel grub efibootmgr linux linux-firmware linux-headers sof-firmware git nano"
+echo "networkmanager base base-devel grub efibootmgr linux linux-firmware linux-headers avahi xdg-user-dirs xdg-utils nfs-utils bash-completion reflector iwd sof-firmware git nano"
 
 echo #
 
@@ -75,7 +41,7 @@ while true; do
 	read -p "Do you want to install Base System? Choose no to make changes to your previous install:(y/n) " yn
 	case $yn in 
 		[yY] ) echo Installing Base System;
-		pacstrap /mnt base base-devel grub efibootmgr linux linux-firmware linux-headers sof-firmware git nano konsole
+		pacstrap /mnt networkmanager base base-devel grub efibootmgr linux linux-firmware linux-headers avahi xdg-user-dirs xdg-utils nfs-utils bash-completion reflector iwd sof-firmware git nano
 		echo #
 		echo #
 		echo "Generate File System Table (fstab)"
@@ -90,18 +56,36 @@ while true; do
 		# Copy the ch2.sh into the arch-chroot environment
 		cp ch2.sh /mnt/ch2.sh
 		cp logos.sh /mnt/logos.sh
+		cp DSS.sh /mnt/DSS.sh
+		cp ff.sh /mnt/ff.sh
+		cp start.sh /mnt/start.sh
 		# Enter the arch-chroot environment and execute the chroot script
 		arch-chroot /mnt /bin/bash /ch2.sh
 		break;;
 		[nN] ) echo ok...we will proceed without installing base system;
-		arch-chroot	
-   		break;;
+			break;;
 		* ) echo invalid response;;
 	esac
 
 	done
+#Phase 2 lets install desktop environment 
+
+deLogo
+
+echo #
+echo #
+DE_menu
+DE_select
+
+Detect_VideoCard
 
 theEndLogo
 echo #
-echo "Installation Complete. Final steps. Install nvidia drivers and a desktop environmnet of your choice."
+echo "Installation Complete.....Do you want to reboot."
+#function ReSetAll () {
+#
+#Your reset function here.
+#
+#}
+
 
